387685c820b715fac08be607170cd66dc972674e
Khodidas Diyora <khodidasdiyora@gmail.com>
2017-03-08 12:39:33 +0530

    Create whileloop.c

---
+++ whileloop.c
@@ -0,0 +1,11 @@
+/* This is an example of a "while" loop */
+void main( )
+{
+  int count;
+  count = 0;
+  while (count < 6) 
+  {
+    printf("The value of count is %d\n",count);
+    count = count + 1;
+  }
+}
